613c0c8281ba4368e963df35ddcd632460b3d531
Nguyễn Minh Quân <81757711+ngminhquan@users.noreply.github.com>
2023-02-23 09:55:51 +0700

    Add files via upload

--- test_system/id_generate/PU_gen/key_gen.py
+++ test_system/id_generate/PU_gen/key_gen.py
@@ -1,1 +1,10 @@
+import rsa
 
+# Generate a 2048-bit RSA key pair
+(pubkey, privkey) = rsa.newkeys(512)
+
+# Print the public key in PEM format
+print(pubkey.save_pkcs1().decode('utf-8'), '\n\n\n')
+
+# Print the private key in PEM format
+print(privkey.save_pkcs1().decode('utf-8'))
